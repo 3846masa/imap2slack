dc0b3d0a53b843ecea9326c1d7566e62b8797f95
3846masa <3846masahiro+git@gmail.com>
2016-05-26 11:29:13 +0900

    Fix bug

--- utils/inbox-wrapper.js
+++ utils/inbox-wrapper.js
@@ -4,7 +4,6 @@ const inbox = require('inbox')
 const pify = require('pify')
 const EventEmitter = require('events')
 const MailParser = require('mailparser').MailParser
-const mailparser = new MailParser()
 
 class Imap extends EventEmitter {
 
@@ -21,7 +20,7 @@ class Imap extends EventEmitter {
   connect () {
     return new Promise((resolve, reject) => {
       this.imap.once('connect', () => {
-        pify(this.imap.openMailbox.bind(this.imap))('INBOX')
+        pify(this.imap.openMailbox.bind(this.imap))('INBOX', { readOnly: true })
         .then(resolve).catch(reject)
       })
       this.imap.connect()
@@ -40,7 +39,7 @@ class Imap extends EventEmitter {
   _onNew (listener) {
     this.imap.on('new', (msg) => {
       this.imap.createMessageStream(msg.UID)
-        .pipe(mailparser)
+        .pipe(new MailParser())
         .on('end', super.emit.bind(this, 'new'))
     })
     return super.on('new', listener)
